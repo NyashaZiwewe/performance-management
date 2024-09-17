1a426e0c7d3b93dc7f1e2a286e0f4f339bde243e
Nyasha.Ziwewe <nyasha.ziwewe@econet.co.zw>
2024-09-17 20:50:45 +0200

    removed moderated score on output

--- src/main/java/hr/performancemanagement/config/StartupCheck.java
+++ src/main/java/hr/performancemanagement/config/StartupCheck.java
@@ -1,31 +1,31 @@
-package hr.performancemanagement.config;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
-
-import javax.annotation.PostConstruct;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
-@Component
-public class StartupCheck {
-
-    @Value("${server.address:}")
-    private String expectedAddress;
-
-    @PostConstruct
-    public void init() {
-        try {
-            InetAddress localAddress = InetAddress.getLocalHost();
-            String actualAddress = localAddress.getHostAddress();
-
-            if (!actualAddress.equals(expectedAddress)) {
-                System.err.println("ERROR: Application is running on unexpected address but got: " + actualAddress);
-                System.exit(1); // Exit with error code
-            }
-        } catch (UnknownHostException e) {
-            System.err.println("ERROR: Unable to determine the local address.");
-            System.exit(1); // Exit with error code
-        }
-    }
-}
+//package hr.performancemanagement.config;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.stereotype.Component;
+//
+//import javax.annotation.PostConstruct;
+//import java.net.InetAddress;
+//import java.net.UnknownHostException;
+//
+//@Component
+//public class StartupCheck {
+//
+//    @Value("${server.address:}")
+//    private String expectedAddress;
+//
+//    @PostConstruct
+//    public void init() {
+//        try {
+//            InetAddress localAddress = InetAddress.getLocalHost();
+//            String actualAddress = localAddress.getHostAddress();
+//
+////            if (!actualAddress.equals(expectedAddress)) {
+////                System.err.println("ERROR: Application is running on unexpected address but got: " + actualAddress);
+////                System.exit(1); // Exit with error code
+////            }
+//        } catch (UnknownHostException e) {
+//            System.err.println("ERROR: Unable to determine the local address.");
+//            System.exit(1); // Exit with error code
+//        }
+//    }
+//}
