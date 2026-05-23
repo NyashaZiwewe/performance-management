0ec6e85c95981a6592b6f41b8587883749f99315
nyasha.ziwewe <nyasha.ziwewe@econet.co.zw>
2026-05-23 17:05:54 +0200

    Sorted without gears

--- src/main/java/hr/performancemanagement/service/api/ScorecardService.java
+++ src/main/java/hr/performancemanagement/service/api/ScorecardService.java
@@ -14,6 +14,8 @@ public interface ScorecardService {
     Double getScoresByReportingDateAndScorecardId(ReportingDate date, Scorecard scorecard);
     int countPassedScorecardsByPeriodId(ReportingPeriod reportingPeriod);
     int countFailedScorecardsByPeriodId(ReportingPeriod reportingPeriod);
+    List<Double> findAverageAllocatedWeightPerStrategicObjective(ReportingPeriod reportingPeriod);
+    List<Double> findAverageWeightedScorePerStrategicObjective(ReportingPeriod reportingPeriod);
     List<Double> findAverageAllocatedWeightPerStrategicObjective();
     List<Double> findAverageWeightedScorePerStrategicObjective();
     Scorecard getScorecardById(long id);
